c26318e17ad2abc59fe4acc7a7972e10a7da7b54
arpurwar <arpurwar@paypal.com>
2023-03-24 21:24:56 +0530

    put & delete api

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,9 +1,10 @@
-const { getAllMovies, getMovieBasedOnId, createMovie } = require('../controller/movies.controller');
+const { getAllMovies, getMovieBasedOnId, createMovie, updateMovie, deleteMovie } = require('../controller/movies.controller');
 const { validateMovieReqBody } = require('../middleware');
 
 module.exports = (app) => {
     app.get('/mba/api/v1/movies', getAllMovies);   
     app.get('/mba/api/v1/movies/:id', getMovieBasedOnId); 
-    app.post('/mba/api/v1/movies', validateMovieReqBody ,createMovie); 
-
+    app.post('/mba/api/v1/movies', validateMovieReqBody,createMovie); 
+    app.put('/mba/api/v1/movies/:id', validateMovieReqBody, updateMovie);
+    app.delete('/mba/api/v1/movies/:id', deleteMovie);
 }
