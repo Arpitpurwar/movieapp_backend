b5b1378098d98eb525abddbc764d7812e61396d3
arpurwar <arpurwar@paypal.com>
2023-03-31 21:47:08 +0530

    user middleware

--- middleware/index.js
+++ middleware/index.js
@@ -2,9 +2,11 @@
 
 const { validateMovieReqBody } = require('./verifyReqBody');
 const { verifyToken, isAdmin} = require('./auth');
+const {validateUserReqBody} = require('./validateUserReqBody')
 
 module.exports = {
     validateMovieReqBody,
     verifyToken,
-    isAdmin
+    isAdmin,
+    validateUserReqBody
 }
