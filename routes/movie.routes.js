bde599f1739ad9a5bb9e1115ac26a45b14bbc65c
arpurwar <arpurwar@paypal.com>
2023-04-01 11:16:04 +0530

    authenticated movie routes

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,10 +1,10 @@
 const { getAllMovies, getMovieBasedOnId, createMovie, updateMovie, deleteMovie } = require('../controller/movies.controller');
-const { validateMovieReqBody, verifyToken } = require('../middleware');
+const { validateMovieReqBody, verifyToken,isAdmin } = require('../middleware');
 
 module.exports = (app) => {
     app.get('/mba/api/v1/movies',verifyToken , getAllMovies);   
-    app.get('/mba/api/v1/movie/:id', getMovieBasedOnId); 
-    app.post('/mba/api/v1/movie', validateMovieReqBody,createMovie); 
-    app.put('/mba/api/v1/movie/:id', validateMovieReqBody, updateMovie);
-    app.delete('/mba/api/v1/movie/:id', deleteMovie);
+    app.get('/mba/api/v1/movie/:id',verifyToken, getMovieBasedOnId); 
+    app.post('/mba/api/v1/movie',[verifyToken, isAdmin, validateMovieReqBody],createMovie); 
+    app.put('/mba/api/v1/movie/:id',[verifyToken, isAdmin, validateMovieReqBody], updateMovie);
+    app.delete('/mba/api/v1/movie/:id',[verifyToken, isAdmin], deleteMovie);
 }
