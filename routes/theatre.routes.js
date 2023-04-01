8b14d26a7e78c5a5325a0654d05da2e44f9c2d54
arpurwar <arpurwar@paypal.com>
2023-04-01 12:01:40 +0530

    update middleware in theatre

--- routes/theatre.routes.js
+++ routes/theatre.routes.js
@@ -1,13 +1,13 @@
 const { getAllTheatres, getTheatreBasedOnId, createTheatre,
      updateTheatre, deleteTheatre, updateMoviesInTheatre, checkMovieInTheatre } = require('../controller/theatre.controller');
-const {verifyToken,validateTheatreRequestBody} = require('../middleware');
+const {verifyToken,validateTheatreRequestBody, isAdmin} = require('../middleware');
 
 module.exports = (app) => {
     app.get('/mba/api/v1/theatres', verifyToken, getAllTheatres);   
     app.get('/mba/api/v1/theatre/:id',verifyToken, getTheatreBasedOnId); 
-    app.post('/mba/api/v1/theatre',[verifyToken, validateTheatreRequestBody],createTheatre); 
-    app.put('/mba/api/v1/theatre/:id',[verifyToken, validateTheatreRequestBody], updateTheatre);
-    app.delete('/mba/api/v1/theatre/:id',verifyToken, deleteTheatre);
-    app.put('/mba/api/v1/theatre/:id/movies',verifyToken, updateMoviesInTheatre);
+    app.post('/mba/api/v1/theatre',[verifyToken, isAdmin, validateTheatreRequestBody],createTheatre); 
+    app.put('/mba/api/v1/theatre/:id',[verifyToken, isAdmin, validateTheatreRequestBody], updateTheatre);
+    app.delete('/mba/api/v1/theatre/:id',verifyToken, isAdmin, deleteTheatre);
+    app.put('/mba/api/v1/theatre/:id/movies',[verifyToken, isAdmin], updateMoviesInTheatre);
     app.get('/mba/api/v1/theatres/:theatreId/movies/:movieId',verifyToken, checkMovieInTheatre)
 }
