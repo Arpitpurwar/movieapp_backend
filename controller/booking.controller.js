4e916bb1ae4caa185e20a08919f2ff6ae783c7ca
arpurwar <arpurwar@paypal.com>
2023-04-03 21:43:24 +0530

    booking api

--- controller/booking.controller.js
+++ controller/booking.controller.js
@@ -1,5 +1,7 @@
 const {ticketCost} = require('../utils/constant');
 const Booking = require('../models/booking.model');
+const User = require('../models/users.models');
+const {userTypes} = require('../utils/constant')
 
 async function createBooking(req,res){
     const tempBooking = {
@@ -22,7 +24,87 @@ async function createBooking(req,res){
     }
 }
 
+async function getBookingOnId(req,res){
+    try{
+        const bookingData = await Booking.findOne({
+            _id: req.params.id
+        })
+        res.send({
+            msg: 'Booking Data sucessfully fetched',
+            bookingData
+        })
+    }
+    catch(err){
+        res.send({
+            msg: "get Booking on id api got failed",
+            err
+        })
+    }
+
+
+}
+
+async function updateBooking(req,res){
+    const booking = await Booking.findOne({
+        _id: req.params.id
+    });
+
+    if(!booking){
+        return res.send({
+            msg: 'Booking Id does not exist'
+        })
+    }
+
+    booking.theatreId = req.body.theatreId ? req.body.theatreId : booking.theatreId;
+    booking.movieId = req.body.movieId != undefined ? req.body.movieId : booking.movieId;
+    booking.userId = req.body.userId != undefined ? req.body.userId : booking.userId;
+    booking.timing = req.body.timing != undefined ? req.body.timing : booking.timing;
+    booking.noOfSeats = req.body.noOfSeats != undefined ? req.body.noOfSeats : booking.noOfSeats;
+    booking.totalCost = booking.noOfSeats * ticketCost;
+    booking.status = req.body.status != undefined ? req.body.status : booking.status; 
+
+
+    try{
+        const result = await booking.save();
+        res.send({
+            msg: 'Booking Id got updated',
+            result
+        })
+
+    }catch(err){
+        res.send({msg: 'Booking updation failed', err})
+    }
+
+
+}
+
+async function getAllBookings(req,res){
+    let tempQuery = {}
+    const user = await User.findOne({
+        _id: req._id
+    });
+
+    if(user.userType !== userTypes.ADMIN){
+        tempQuery.userId =  req._id;
+    }
+
+    try{
+        const bookingData = await Booking.find(tempQuery);
+        res.send({
+            msg: "All booking fetch successfully",
+            bookingData
+        })
+    }
+    catch(err){
+        res.send({msg: 'get All booking api failed', err})
+    }
+    
+
+}
 
 module.exports = {
-    createBooking
+    createBooking,
+    getBookingOnId,
+    updateBooking,
+    getAllBookings
 }
--- routes/booking.routes.js
+++ routes/booking.routes.js
@@ -8,8 +8,8 @@ const { validateBookingReqBody, verifyToken } = require("../middleware");
  */
 
 module.exports = function (app) {
-   // app.get("/mba/api/v1/bookings", bookingController.getAllBookings); 
-    //app.get("/mba/api/v1/bookings/:id", bookingController.getBookingOnId);  
+    app.get("/mba/api/v1/bookings", verifyToken,bookingController.getAllBookings); 
+    app.get("/mba/api/v1/bookings/:id",verifyToken, bookingController.getBookingOnId);  
     app.post("/mba/api/v1/bookings",[verifyToken,validateBookingReqBody], bookingController.createBooking);
-    //app.put("/mba/api/v1/bookings/:id",  bookingController.updateBooking);
+    app.put("/mba/api/v1/bookings/:id", verifyToken,  bookingController.updateBooking);
 }
